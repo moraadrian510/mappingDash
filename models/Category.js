e948e02838d43c7d3e719e1efb3d7ba7501d330f
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-10 16:38:47 -0800

    added model /product

--- models/Category.js
+++ models/Category.js
@@ -1,28 +1,28 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    Category_id: {
+    id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
-      notNull: true,
+      allowNull: false,
     },
-    category_name : {
+    categoryName: {
       type: DataTypes.STRING,
-      notNull: true,
-    }
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
