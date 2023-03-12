eed0aa45e1a03a5ac50e8543a971a3149cd16e4e
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-03-11 18:43:17 -0800

    added api routes / Tag / added code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,7 +62,7 @@ router.delete("/:id", async (req, res) => {
     const tagData = await Tag.destroy({
       where: { id: req.params.id },
     });
-    res.status(200).json({ message: "Tag deleted successfully" });
+    res.status(200).json({ tagData, message: "Tag deleted successfully" });
   } catch (err) {
     res.status(500).json(err);
   }
